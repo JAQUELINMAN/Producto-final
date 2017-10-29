40c14317bb6ac5036344caa7688a351f75153cf7
jaquelinman <alfilnegrolg@hotmail.com>
2017-10-29 10:49:41 -0600

    Avance de productos finales

---
+++ cifrado-cesar/README.md
@@ -0,0 +1,10 @@
+### Pseudocodigo
+
+##Cifrado Cesar
+
+1. Pedir al usuario por medio de un prompt() una frase.
+2. Devolver al mismo mensaje encriptado.
+3.
+4.
+5.
+
---
+++ cifrado-cesar/js/app.js
@@ -0,0 +1,25 @@
+var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var sentence = prompt ("Dime una frase");
+
+sentence = sentence.toUpperCase();
+
+var sentenceNew = for (var i = 1; i <= it.length; i= i + 2) 
+
+if (sentence && sentenceNew) {
+	alert("Numero invalido");
+} 
+
+console.log ()
+// else {
+// 	var leterNew = dni[number % 26];
+// 	if (leterNew != leter){
+// 		alert("Numero o letra proporcionados incorrectos");
+// 	}
+// 	else {
+// 		alert ("Numer de DNI y letra correctos")
+// 	}
+// }
+
+// var cc = ((dni + number) % 26)
+// console.log (dni.indexOf(cc));
+// 	
